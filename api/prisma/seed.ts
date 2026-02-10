6167fcb6fbf9caa56e8f3df05e36af6d42b54c5c
Carlos Faustino <chfcchfc96@gmail.com>
2026-02-10 12:50:55 -0300

    feat: add prisma seed and hours basis

---
+++ api/prisma/seed.ts
@@ -0,0 +1,87 @@
+import { faker } from "@faker-js/faker"
+import { hash } from "argon2"
+
+import { prisma } from "@/lib/prisma"
+import { hours } from "@/utils/hours"
+
+export async function main() {
+  const password = await hash("123456")
+
+  await prisma.user.create({
+    data: {
+      name: "Carlos",
+      email: "carlos@email.com",
+      password,
+      role: "ADMIN",
+    },
+  })
+
+  const technicians = await prisma.user.createManyAndReturn({
+    data: [
+      {
+        name: "Tech 1",
+        email: "tech1@email.com",
+        password,
+        role: "TECHNICIAN",
+      },
+      {
+        name: "Tech 2",
+        email: "tech2@email.com",
+        password,
+        role: "TECHNICIAN",
+      },
+      {
+        name: "Tech 3",
+        email: "tech3@email.com",
+        password,
+        role: "TECHNICIAN",
+      },
+    ],
+  })
+
+  await prisma.user.create({
+    data: {
+      name: "Client",
+      email: "client@email.com",
+      password,
+      role: "CLIENT",
+    },
+  })
+
+  const technicianAvailability = technicians.flatMap((technician, index) => {
+    const init = index * 5
+    const end = init + 5
+
+    const technicianHours = hours.slice(init, end)
+
+    return technicianHours.map(hour => ({
+      hour,
+      technicianId: technician.id,
+    }))
+  })
+
+  await prisma.technicianAvailability.createMany({
+    data: technicianAvailability,
+  })
+
+  const serviceName = [
+    "Instalação e atualização de softwares",
+    "Instalação e atualização de hardwares",
+    "Diagnóstico e remoção de vírus",
+    "Suporte a impressoras",
+    "Suporte a periféricos",
+    "Solução de problemas de conectividade de internet",
+    "Backup e recuperação de dados",
+    "Otimização de desempenho do sistema operacional",
+    "Configuração de VPN e Acesso Remoto",
+  ]
+
+  await prisma.service.createManyAndReturn({
+    data: serviceName.map(item => ({
+      name: item,
+      price: faker.commerce.price(),
+    })),
+  })
+}
+
+main()
