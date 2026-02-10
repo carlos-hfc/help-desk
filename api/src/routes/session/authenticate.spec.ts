71a299dc8aeb525a15a44df945dc37abdbe8eaa4
Carlos Faustino <chfcchfc96@gmail.com>
2026-02-10 14:02:27 -0300

    test: add authenticate test

---
+++ api/src/routes/session/authenticate.spec.ts
@@ -0,0 +1,58 @@
+import request from "supertest"
+
+import { app } from "@/app"
+import { makeUser } from "@/test/make-user"
+
+describe("Authenticate user [POST] /sessions/authenticate", () => {
+  it("should be able to authenticate an user", async () => {
+    const user = makeUser()
+
+    await request(app.server).post("/sessions/register").send(user)
+
+    const response = await request(app.server)
+      .post("/sessions/authenticate")
+      .send({
+        email: user.email,
+        password: user.password,
+      })
+
+    expect(response.status).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+      role: expect.any(String),
+    })
+    expect(response.get("Set-Cookie")).toEqual([expect.any(String)])
+  })
+
+  it("should not be able to authenticate an user with wrong e-mail", async () => {
+    const user = makeUser()
+
+    await request(app.server).post("/sessions/register").send(user)
+
+    const response = await request(app.server)
+      .post("/sessions/authenticate")
+      .send({
+        email: "email@email.com",
+        password: user.password,
+      })
+
+    expect(response.status).toEqual(400)
+    expect(response.body).toHaveProperty("message", "Invalid credentials")
+  })
+
+  it("should not be able to authenticate an user with wrong password", async () => {
+    const user = makeUser()
+
+    await request(app.server).post("/sessions/register").send(user)
+
+    const response = await request(app.server)
+      .post("/sessions/authenticate")
+      .send({
+        email: user.email,
+        password: "user.password",
+      })
+
+    expect(response.status).toEqual(400)
+    expect(response.body).toHaveProperty("message", "Invalid credentials")
+  })
+})
