6f080062c550c4d144a41b7b66ed19eecf49b83b
Carlos Faustino <chfcchfc96@gmail.com>
2026-02-11 11:41:17 -0300

    refactor: reset db before all tests

--- api/setup-test.ts
+++ api/setup-test.ts
@@ -1,6 +1,24 @@
 import { app } from "@/app"
+import { Prisma } from "@/generated/prisma/client"
+import { prisma } from "@/lib/prisma"
 
 beforeAll(async () => {
+  const tablenames = await prisma.$queryRaw<Array<{ tablename: string }>>(
+    Prisma.sql`SELECT tablename FROM pg_tables WHERE schemaname='public'`,
+  )
+
+  const tables = tablenames
+    .map(({ tablename }) => tablename)
+    .filter(name => name !== "_prisma_migrations")
+    .map(name => `"public"."${name}"`)
+    .join(", ")
+
+  try {
+    await prisma.$executeRawUnsafe(`TRUNCATE TABLE ${tables} CASCADE;`)
+  } catch (error) {
+    console.log({ error })
+  }
+
   await app.ready()
 })
 
