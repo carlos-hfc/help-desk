d1a87a6a95653f851ec70d81c6206bf5ee47ef07
Carlos Faustino <chfcchfc96@gmail.com>
2026-02-18 14:29:00 -0300

    feat: update dashboard page and app layout

--- web/src/layouts/app-layout.tsx
+++ web/src/layouts/app-layout.tsx
@@ -8,7 +8,7 @@ export function AppLayout() {
       <Header />
 
       <div className="w-full lg:w-[calc(100%-210px)] absolute right-0 top-24 lg:top-3 bg-gray-600 rounded-3xl lg:rounded-tl-3xl lg:rounded-none">
-        <div className="h-dvh">
+        <div className="h-dvh p-6 lg:p-12 flex flex-col gap-6">
           <Outlet />
         </div>
       </div>
--- web/src/pages/dashboard/index.tsx
+++ web/src/pages/dashboard/index.tsx
@@ -1,3 +1,23 @@
+import { PageTitle } from "@/components/page-title"
+import { Table, TableBody } from "@/components/table"
+
+import { CallsTableHead } from "./calls-table-head"
+import { CallsTableRow } from "./calls-table-row"
+
 export function Dashboard() {
-  return <div>dashboard</div>
+  return (
+    <>
+      <PageTitle title="Chamados" />
+
+      <Table>
+        <CallsTableHead />
+
+        <TableBody className="[&_tr:last-child]:border-0">
+          {Array.from({ length: 5 }).map((_, i) => (
+            <CallsTableRow key={i} />
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  )
 }
