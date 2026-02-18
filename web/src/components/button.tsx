d0513b523bad05e043b6dcce3af448ad1339e53f
Carlos Faustino <chfcchfc96@gmail.com>
2026-02-18 12:38:10 -0300

    refactor: update button size with svg

--- web/src/components/button.tsx
+++ web/src/components/button.tsx
@@ -18,13 +18,13 @@ export function Button({
       {...props}
       className={cn(
         "rounded-sm flex items-center justify-center gap-2 font-bold text-sm cursor-pointer disabled:pointer-events-none",
+        size === "md" && "px-4 h-10 [&>svg]:size-4.5",
+        size === "sm" && "px-2 h-7 [&>svg]:size-3.5",
         variant === "primary" && "bg-gray-200 text-gray-600 hover:bg-gray-100",
         variant === "secondary" &&
           "bg-gray-500 text-gray-200 hover:bg-gray-400 hover:text-gray-100",
         variant === "link" &&
-          "bg-white text-gray-300 hover:bg-gray-500 hover:text-gray-100",
-        size === "md" && "px-4 h-10",
-        size === "sm" && "px-2 h-7",
+          "bg-white text-gray-300 hover:bg-gray-500 hover:text-gray-100 px-0.5",
         icon && size === "md" && "p-0 size-10",
         icon && size === "sm" && "p-0 size-7",
         className,
