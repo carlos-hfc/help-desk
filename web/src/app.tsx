b9600af2d57eb36ca1c082ea707f9d367eb0a5d7
Carlos Faustino <chfcchfc96@gmail.com>
2026-02-19 16:18:56 -0300

    feat: add libs config

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,7 +1,13 @@
+import { QueryClientProvider } from "@tanstack/react-query"
 import { RouterProvider } from "react-router"
 
+import { queryClient } from "./lib/react-query"
 import { routes } from "./routes"
 
 export function App() {
-  return <RouterProvider router={routes} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routes} />
+    </QueryClientProvider>
+  )
 }
