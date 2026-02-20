6d46d428abeee1c5b32b9980a04b1cd10da82d0d
Carlos Faustino <chfcchfc96@gmail.com>
2026-02-20 13:15:40 -0300

    refactor: add password validation on update profile

--- api/src/routes/user/update-profile.ts
+++ api/src/routes/user/update-profile.ts
@@ -1,10 +1,15 @@
-import { hash } from "argon2"
+import { hash, verify } from "argon2"
 import { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
 import z from "zod"
 
+import { ClientError } from "@/errors/client-error"
 import { prisma } from "@/lib/prisma"
 import { auth } from "@/middlewares/auth"
-import { NoContentSchema } from "@/utils/global-response-schema"
+import {
+  BadRequestSchema,
+  NoContentSchema,
+  NotFoundSchema,
+} from "@/utils/global-response-schema"
 
 export const updateProfile: FastifyPluginAsyncZod = async app => {
   app.register(auth).put(
@@ -14,22 +19,64 @@ export const updateProfile: FastifyPluginAsyncZod = async app => {
         tags: ["profile"],
         summary: "Update profile from logged user",
         security: [{ cookieAuth: [] }],
-        body: z.object({
-          name: z.string().optional(),
-          email: z.email().optional(),
-          password: z.string().min(6).optional(),
-        }),
+        body: z
+          .object({
+            name: z.string().nonempty().optional(),
+            email: z.email().optional(),
+            password: z.string().min(6).optional(),
+            currentPassword: z.string().min(6).optional(),
+          })
+          .refine(
+            data =>
+              data.password || data.currentPassword
+                ? data.password && data.currentPassword
+                : true,
+            {
+              message: "Current password and new password are required",
+              path: ["currentPassword", "password"],
+            },
+          ),
         response: {
           204: NoContentSchema,
+          400: BadRequestSchema,
+          404: NotFoundSchema,
         },
       },
     },
     async (request, reply) => {
       const { id } = await request.getCurrentUser()
 
-      const { email, name, password } = request.body
+      const { email, name, password, currentPassword } = request.body
 
-      const passwordHash = password ? await hash(password) : undefined
+      let passwordHash: string | undefined
+
+      const user = await prisma.user.findUnique({
+        where: { id },
+      })
+
+      if (!user) {
+        throw new ClientError("User not found", 404)
+      }
+
+      if (currentPassword) {
+        const doesPasswordMatch = await verify(user.password, currentPassword)
+
+        if (!doesPasswordMatch) {
+          throw new ClientError("The current password does not match")
+        }
+      }
+
+      if (password) {
+        const doesPasswordMatch = await verify(user.password, password)
+
+        if (doesPasswordMatch) {
+          throw new ClientError(
+            "New password cannot be equal to the current password",
+          )
+        }
+
+        passwordHash = await hash(password)
+      }
 
       await prisma.user.update({
         where: { id },
