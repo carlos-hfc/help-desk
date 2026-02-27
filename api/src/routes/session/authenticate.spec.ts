d35a0668297d55c169b0e4c7463a60dcbb33b2f3
Carlos Faustino <chfcchfc96@gmail.com>
2026-02-27 15:00:23 -0300

    test: register client users before authenticating

--- api/src/routes/session/authenticate.spec.ts
+++ api/src/routes/session/authenticate.spec.ts
@@ -5,10 +5,9 @@ import { makeUser } from "@/test/make-user"
 
 describe("Authenticate user [POST] /sessions/authenticate", () => {
   it("should be able to authenticate an user", async () => {
-    const user = makeUser()
+    const user = makeUser({ role: "CLIENT" })
 
     await request(app.server).post("/sessions/register").send(user)
-    await request(app.server).post("/sessions/create-password").send(user)
 
     const response = await request(app.server)
       .post("/sessions/authenticate")
