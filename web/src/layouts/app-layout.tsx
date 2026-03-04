f135bc08b3e1ef508ddab335d3bce659e534f5d4
Carlos Faustino <chfcchfc96@gmail.com>
2026-03-04 17:43:26 -0300

    refactor: update background color

--- web/src/layouts/app-layout.tsx
+++ web/src/layouts/app-layout.tsx
@@ -30,11 +30,11 @@ export function AppLayout() {
   }, [navigate])
 
   return (
-    <div className="min-h-dvh flex flex-col lg:flex-row relative">
+    <div className="min-h-dvh flex flex-col lg:flex-row relative bg-gray-600">
       <Header />
 
-      <div className="w-full lg:w-[calc(100%-210px)] absolute right-0 bottom-0 h-[calc(100%-6rem)] lg:h-[calc(100%-.75rem)] bg-gray-600 rounded-t-3xl lg:rounded-tl-3xl lg:rounded-none">
-        <div className="p-6 lg:p-12 flex flex-col gap-6">
+      <div className="w-full lg:w-[calc(100%-210px)] absolute right-0 bottom-0 h-[calc(100%-6rem)] lg:h-[calc(100%-.75rem)] bg-gray-600 rounded-t-3xl lg:rounded-tl-3xl lg:rounded-tr-none">
+        <div className="p-6 lg:p-12 flex flex-col gap-6 rounded-t-3xl lg:rounded-tl-3xl lg:rounded-tr-none">
           <Outlet />
         </div>
       </div>
