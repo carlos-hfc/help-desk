6cd2603379a3eab1225171611e6a06f4a8e45112
Carlos Faustino <chfcchfc96@gmail.com>
2026-03-04 17:44:18 -0300

    refactor: update font size in differents button sizes

--- web/src/components/button.tsx
+++ web/src/components/button.tsx
@@ -17,14 +17,14 @@ export function Button({
     <button
       {...props}
       className={cn(
-        "rounded-sm flex items-center justify-center gap-2 font-bold text-sm cursor-pointer disabled:opacity-70 disabled:pointer-events-none transition",
-        size === "md" && "px-4 h-10 [&>svg]:size-4.5",
-        size === "sm" && "px-2 h-7 [&>svg]:size-3.5",
+        "rounded-sm flex items-center justify-center gap-2 font-bold cursor-pointer disabled:opacity-70 disabled:pointer-events-none transition",
+        size === "md" && "px-4 h-10 text-sm [&>svg]:size-4.5",
+        size === "sm" && "px-2 h-7 text-xs [&>svg]:size-3.5",
         variant === "primary" && "bg-gray-200 text-gray-600 hover:bg-gray-100",
         variant === "secondary" &&
           "bg-gray-500 text-gray-200 hover:bg-gray-400 hover:text-gray-100",
         variant === "link" &&
-          "bg-white text-gray-300 hover:bg-gray-500 hover:text-gray-100 px-0.5",
+          "bg-gray-600 text-gray-300 hover:bg-gray-500 hover:text-gray-100 px-0.5",
         icon && size === "md" && "p-0 size-10",
         icon && size === "sm" && "p-0 size-7",
         icon && variant === "link" && "size-6",
